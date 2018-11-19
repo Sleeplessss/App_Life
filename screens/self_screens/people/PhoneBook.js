d50dbfed799e00b56533c5985f97ebbc69c7e325
SAKDA <sakda.ptcp@gmail.com>
2018-11-19 20:42:34 +0700

    add phone book

---
+++ screens/self_screens/people/PhoneBook.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+class PhoneBook extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.AddNameList}> รายชื่อ </Text>
+      </View>
+    );
+  }
+}
+const styles = StyleSheet.create({
+    container: {
+        flex:1,
+        backgroundColor: '#ffffff',
+        alignContent: 'center',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    AddNameList: {
+        fontSize:25,
+        color:'#455A64'
+    }
+});
+export default PhoneBook;
