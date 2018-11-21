f8b7f6d8014b8772c73b9a6c16dc431bb9a1cd11
SAKDA <sakda.ptcp@gmail.com>
2018-11-21 13:25:22 +0700

    no message

--- screens/self_screens/people/PhoneBook.js
+++ screens/self_screens/people/PhoneBook.js
@@ -45,9 +45,6 @@ const styles = StyleSheet.create({
     container: {
         flex:1,
         backgroundColor: '#ffffff',
-        // alignContent: 'center',
-        // alignItems: 'center',
-        // justifyContent: 'center'
     },
 });
 export default PhoneBook;
